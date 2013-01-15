dc12a64515cc585cc1bf0877680d13a714d36d92
Trevor Nielsen <95010562@gh-lab-l14a-15.palo-alto.edu>
2013-01-15 09:04:22 -0800

    sup yall

--- Main.java
+++ Main.java
@@ -3,27 +3,15 @@ public class Main
 
 	public static void main(String args[])
 	{
-		BST<Integer> myTree = new BST<Integer>();
+		BST<String> myTree = new BST<String>();
 
-		myTree.insert(new Integer(2));
-		myTree.insert(new Integer(1));
-		myTree.insert(new Integer(9));
-		myTree.insert(new Integer(0));
-		myTree.insert(new Integer(2));
-		myTree.insert(new Integer(9));
-		myTree.insert(new Integer(7));
-		myTree.insert(new Integer(10));
-		
-		myTree.printTree();
-
-		myTree.delete(new Integer(10));
-		myTree.delete(new Integer(1));
-
-		myTree.printTree();
-
-		myTree.insert(new Integer(10));
-		myTree.insert(new Integer(2000));
-
+		myTree.insert("Adam");
+		myTree.insert("Eve");
+		myTree.insert("Romeo");
+		myTree.insert("Juliet");
+		myTree.insert("Tom");
+		myTree.insert("Dick");
+		myTree.insert("Harry");
 		myTree.printTree();
 	}
 }
